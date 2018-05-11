21c71b05fafd0ec78a66cb7ebeb002460ca1a8d9
Joshua Murphy <joshmurf@live.co.uk>
2018-05-11 01:20:01 +0100

    Added a Blank JFrame window

---
+++ src/ballGame/Main.java
@@ -0,0 +1,15 @@
+package ballGame;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args){
+        JFrame Window = new JFrame();
+        Window.setBounds(100,100,800,500);
+        Window.setTitle("Ball Game");
+        Window.setResizable(false);
+        Window.setVisible(true);
+        Window.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+    }
+}
